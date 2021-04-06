fbdef9c03c2f3db54fe65d3b66fd16c87ade40dc
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-04-06 16:15:38 +0800

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -27,6 +27,7 @@ type StatusResult<T> =
 interface Resource<T> {
   read(): T;
   load(): void;
+  get(): StatusResult<T> | undefined;
 }
 
 function createResource<T>(provider: () => Promise<T>): Resource<T> {
@@ -35,6 +36,23 @@ function createResource<T>(provider: () => Promise<T>): Resource<T> {
   const listeners = new Set<() => void>();
 
   return {
+    get(): StatusResult<T> | undefined {
+      const [, setState] = useState([]);
+
+      useEffect(() => {
+        const onUpdate = () => {
+          setState([]);
+        };
+
+        listeners.add(onUpdate);
+
+        return () => {
+          listeners.delete(onUpdate);
+        };
+      }, []);
+
+      return status;
+    },
     read(): T {
       const [, setState] = useState([]);
 
@@ -56,10 +74,10 @@ function createResource<T>(provider: () => Promise<T>): Resource<T> {
           status: 'pending',
           value: provider().then(
             (value) => {
-              status = {
-                status: 'success',
-                value,
-              };
+                status = {
+                  status: 'success',
+                  value,
+                };
             },
             (value) => {
               status = {
@@ -137,12 +155,42 @@ function Details() {
 //   );
 // }
 
+function Status() {
+  const status = resource.get();
+
+  if (status) {
+    return <h1>Status: {status.status}</h1>
+  }
+
+  return <h1>Not loaded</h1>;
+}
+
+
+// This component may indirectly contribute to the error.
+function Clock() {
+  const [state, setState] = useState(0);
+
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      setState((state) => state + 1);
+    }, 1000); 
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  return <h1>Time: {state}</h1>
+}
+
 export default function App() {
   return (
     <>
       <button type="button" onClick={resource.load}>
         Reload (Click rapidly to reproduce error)
       </button>
+      <Status />
+      <Clock />
       <h1>Single Child</h1>
       <Suspense fallback={<h1>Loading...</h1>}>
         <Details />
